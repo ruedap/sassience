3cd1c8b18f209931b608c38991f8928acf1469d1
ruedap <ruedap@ruedap.com>
2014-08-16 00:59:54 +0900

    Improve convert result page

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -2,18 +2,18 @@ class SheetsController < ApplicationController
   # GET /sheets/new
   def new
     @sheet = Sheet.new
+    @converted_sheet = converted_sheet
   end
 
   # POST /sheets
   def create
     @sheet = Sheet.new(sheet_params)
 
-    respond_to do |format|
-      if @sheet.save
-        format.html { redirect_to root_path, notice: 'Sheet was successfully created.' }
-      else
-        format.html { render :new }
-      end
+    if @sheet.save
+      session[:converted_sheet_id] = @sheet.id
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
@@ -22,5 +22,11 @@ class SheetsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def sheet_params
     params.require(:sheet).permit(:original_code)
+  end
+
+  def converted_sheet
+    sheet_id = session[:converted_sheet_id]
+    session[:converted_sheet_id] = nil
+    sheet_id ? Sheet.find(sheet_id) : nil
   end
 end
