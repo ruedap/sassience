d4d5f6d6354271fa2406f345a9d499b2c149c5b5
ruedap <ruedap@ruedap.com>
2015-04-28 16:59:08 +0900

    Implement basic error handling

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -3,6 +3,7 @@ class SheetsController < ApplicationController
   def new
     @sheet = Sheet.new
     @converted_sheet = converted_sheet
+    @conversion_error = conversion_error
     @sheet.original_code = @converted_sheet.converted_code if @converted_sheet
   end
 
@@ -10,7 +11,17 @@ class SheetsController < ApplicationController
   def create
     @sheet = Sheet.new(sheet_params)
     @sheet.detect_convert_type(params[:convert_type_1])
-    @sheet.convert
+
+    begin
+      @sheet.convert
+    rescue Sass::SyntaxError => e
+      error_line = e.backtrace.first.scan(/[0-9]+/).last
+      error_line_num = error_line ? error_line.scan(/./) : nil
+      error_type = error_line ? 1 : 0
+      session[:conversion_error] = { type: error_type, line_num: error_line_num }
+    rescue
+      session[:conversion_error] = { type: 0 }
+    end
 
     if @sheet.save
       session[:converted_sheet_id] = @sheet.id
@@ -35,5 +46,11 @@ class SheetsController < ApplicationController
     sheet_id = session[:converted_sheet_id]
     session[:converted_sheet_id] = nil
     sheet_id ? Sheet.find(sheet_id) : nil
+  end
+
+  def conversion_error
+    error_obj = session[:conversion_error]
+    session[:conversion_error] = nil
+    error_obj
   end
 end
