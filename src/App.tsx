cc279841e9b377b893104fb8f6f688218fc75638
ruedap <ruedap@ruedap.com>
2020-08-18 00:03:55 +0900

    feat(src): Style top page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>This site is temporarily unavailable.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ruedap/sassience"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ruedap/sassience
         </a>
       </header>
     </div>
